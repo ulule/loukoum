383d9dfc690dc0e350b7b7a0df2b59325e7bff2d
Thomas LE ROUX <thomas@leroux.io>
2017-11-02 16:47:17 +0100

    feat: add a simple lexer

---
+++ lexer/lexer.go
@@ -0,0 +1,185 @@
+package lexer
+
+import (
+	"bufio"
+	"io"
+
+	"github.com/ulule/loukoum/token"
+)
+
+const (
+	// EOF sentinel
+	eof = 0
+	// Newline sentinel
+	newline = '\n'
+)
+
+// A Lexer will return a list of Token from a reader.
+type Lexer struct {
+	input  *bufio.Reader
+	e0, en rune // current/next rune in reader
+}
+
+// New return a new Lexer from given source.
+func New(input io.Reader) Lexer {
+
+	buffer := bufio.NewReader(input)
+
+	l := Lexer{}
+	l.input = buffer
+
+	// Read two runes so current and next items are both available.
+	l.read()
+	l.read()
+
+	return l
+}
+
+func (l *Lexer) read() rune {
+	e, _, err := l.input.ReadRune()
+
+	if err != nil && err != io.EOF {
+		//l.error(err.Error())
+		// TODO FIX ME
+		panic(err)
+	}
+
+	if err == io.EOF {
+		e = eof
+	}
+
+	l.e0 = l.en
+
+	l.en = e
+	return l.e0
+}
+
+// current return the current rune in reader.
+func (l *Lexer) current() rune {
+	return l.e0
+}
+
+// next return the next rune in reader.
+func (l *Lexer) next() rune {
+	return l.en
+}
+
+// Next will return the next token on reader.
+func (l *Lexer) Next() token.Token {
+
+	l.skipWhitespace()
+
+	if l.current() == eof {
+		return l.getToken(token.EOF)
+	}
+
+	if l.current() == newline {
+		return l.getToken(token.Semicolon)
+	}
+
+	t, ok := l.getOperatorToken()
+	if ok {
+		return t
+	}
+
+	t, ok = l.getDelimiterToken()
+	if ok {
+		return t
+	}
+
+	return l.getDefaultToken()
+}
+
+func (l *Lexer) getToken(t token.Type) token.Token {
+	defer l.read()
+	switch t {
+	case token.EOF:
+		return token.New(t, "")
+	case token.Semicolon:
+		return token.New(t, ";")
+	default:
+		return token.New(t, string(l.current()))
+	}
+}
+
+func (l *Lexer) getOperatorToken() (token.Token, bool) {
+	switch l.current() {
+	case '*':
+		return l.getToken(token.Asterisk), true
+	case '=':
+		return l.getToken(token.Equals), true
+	default:
+		return token.Token{}, false
+	}
+}
+
+func (l *Lexer) getDelimiterToken() (token.Token, bool) {
+	switch l.current() {
+	case ';':
+		return l.getToken(token.Semicolon), true
+	case ',':
+		return l.getToken(token.Comma), true
+	case ':':
+		return l.getToken(token.Colon), true
+	default:
+		return token.Token{}, false
+	}
+}
+
+func (l *Lexer) getDefaultToken() token.Token {
+
+	if isLetter(l.current()) || isDigit(l.current()) {
+		return l.getIdentifier()
+	}
+
+	return l.getToken(token.Illegal)
+}
+
+func (l *Lexer) getIdentifier() token.Token {
+
+	t := token.Token{}
+	v, ok := l.readIdentifier()
+
+	t.Value = v
+	t.Type = token.Lookup(t.Value)
+
+	if !ok {
+		t.Type = token.Illegal
+	}
+
+	return t
+}
+
+func (l *Lexer) skipWhitespace() {
+	for isWhitespace(l.current()) {
+		l.read()
+	}
+}
+
+func (l *Lexer) readIdentifier() (string, bool) {
+
+	buffer := []rune{}
+
+	for isLetter(l.current()) || isDigit(l.current()) {
+		buffer = append(buffer, l.current())
+		l.read()
+	}
+
+	return string(buffer), true
+}
+
+func isLetter(e rune) bool {
+	return 'a' <= e && e <= 'z' || 'A' <= e && e <= 'Z' || e == '_' || e == '.'
+}
+
+func isWhitespace(e rune) bool {
+	return e == ' ' || e == '\t' || e == '\n' || e == '\r'
+}
+
+func isDigit(e rune) bool {
+	return '0' <= e && e <= '9'
+}
+
+func isHex(e rune) bool {
+	return isDigit(e) || 'a' <= e && e <= 'f' || 'A' <= e && e <= 'F'
+}
---
+++ lexer/lexer_test.go
@@ -0,0 +1,160 @@
+package lexer_test
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/ulule/loukoum/lexer"
+	"github.com/ulule/loukoum/token"
+)
+
+type LexScenario struct {
+	Input  string
+	Tokens []token.Token
+}
+
+func execute(t *testing.T, scenarios []LexScenario) {
+	is := require.New(t)
+
+	for i, scenario := range scenarios {
+
+		l := lexer.New(strings.NewReader(scenario.Input))
+
+		for y, expected := range scenario.Tokens {
+			message := fmt.Sprintf("Scenario #%d / Token #%d", (i + 1), (y + 1))
+
+			actual := l.Next()
+
+			is.Equal(expected.Type, actual.Type, message)
+			is.Equal(expected.Value, actual.Value, message)
+		}
+
+		actual := l.Next()
+		is.Equal(token.EOF, actual.Type, "EOF was expected")
+	}
+}
+
+func TestNextToken(t *testing.T) {
+
+	tests := []LexScenario{}
+
+	// Scenario #1: Check EOF on empty source
+	tests = append(tests, LexScenario{
+		``,
+		[]token.Token{
+			token.New(token.EOF, ""),
+			token.New(token.EOF, ""),
+			token.New(token.EOF, ""),
+			token.New(token.EOF, ""),
+			token.New(token.EOF, ""),
+			token.New(token.EOF, ""),
+			token.New(token.EOF, ""),
+			token.New(token.EOF, ""),
+			token.New(token.EOF, ""),
+			token.New(token.EOF, ""),
+			token.New(token.EOF, ""),
+			token.New(token.EOF, ""),
+		},
+	})
+
+	// Scenario #2: A simple SELECT query
+	tests = append(tests, LexScenario{
+		`
+			SELECT * FROM foobar WHERE id = 2;
+		`,
+		[]token.Token{
+			token.New(token.Select, "SELECT"),
+			token.New(token.Asterisk, "*"),
+			token.New(token.From, "FROM"),
+			token.New(token.Literal, "foobar"),
+			token.New(token.Where, "WHERE"),
+			token.New(token.Literal, "id"),
+			token.New(token.Equals, "="),
+			token.New(token.Literal, "2"),
+			token.New(token.Semicolon, ";"),
+		},
+	})
+
+	// Scenario #3: Another simple SELECT query with multiples columns
+	tests = append(tests, LexScenario{
+		`
+			SELECT a,b,c FROM foobar;
+		`,
+		[]token.Token{
+			token.New(token.Select, "SELECT"),
+			token.New(token.Literal, "a"),
+			token.New(token.Comma, ","),
+			token.New(token.Literal, "b"),
+			token.New(token.Comma, ","),
+			token.New(token.Literal, "c"),
+			token.New(token.From, "FROM"),
+			token.New(token.Literal, "foobar"),
+			token.New(token.Semicolon, ";"),
+		},
+	})
+
+	// Scenario #4: Detect if newline is ignored
+	tests = append(tests, LexScenario{
+		`
+			SELECT a, b, c FROM foobar
+		`,
+		[]token.Token{
+			token.New(token.Select, "SELECT"),
+			token.New(token.Literal, "a"),
+			token.New(token.Comma, ","),
+			token.New(token.Literal, "b"),
+			token.New(token.Comma, ","),
+			token.New(token.Literal, "c"),
+			token.New(token.From, "FROM"),
+			token.New(token.Literal, "foobar"),
+		},
+	})
+
+	// Scenario #5: Detect if EOF is ignored
+	tests = append(tests, LexScenario{
+		`SELECT a, b, c FROM foobar`,
+		[]token.Token{
+			token.New(token.Select, "SELECT"),
+			token.New(token.Literal, "a"),
+			token.New(token.Comma, ","),
+			token.New(token.Literal, "b"),
+			token.New(token.Comma, ","),
+			token.New(token.Literal, "c"),
+			token.New(token.From, "FROM"),
+			token.New(token.Literal, "foobar"),
+		},
+	})
+
+	// Scenario #6: A subquery using an INNER JOIN
+	tests = append(tests, LexScenario{
+		`INNER JOIN test2 ON test2.id = test.fk_id`,
+		[]token.Token{
+			token.New(token.Inner, "INNER"),
+			token.New(token.Join, "JOIN"),
+			token.New(token.Literal, "test2"),
+			token.New(token.On, "ON"),
+			token.New(token.Literal, "test2.id"),
+			token.New(token.Equals, "="),
+			token.New(token.Literal, "test.fk_id"),
+		},
+	})
+
+	// Scenario #7: A simple delete query
+	tests = append(tests, LexScenario{
+		`DELETE FROM test2 WHERE id = 5`,
+		[]token.Token{
+			token.New(token.Delete, "DELETE"),
+			token.New(token.From, "FROM"),
+			token.New(token.Literal, "test2"),
+			token.New(token.Where, "WHERE"),
+			token.New(token.Literal, "id"),
+			token.New(token.Equals, "="),
+			token.New(token.Literal, "5"),
+		},
+	})
+
+	execute(t, tests)
+}
