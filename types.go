c02485efd9e9355624800f8023bc7fb7fdf92950
Thomas LE ROUX <thomas@leroux.io>
2017-11-10 17:48:51 +0100

    refactor: update wrapper

--- types.go
+++ types.go
@@ -12,10 +12,6 @@ const (
 	LeftJoin = types.LeftJoin
 	// RightJoin is used for "RIGHT JOIN" in join statement.
 	RightJoin = types.RightJoin
-	// And is the AND logical operator.
-	And = types.And
-	// Or is the Or logical operator.
-	Or = types.Or
 )
 
 // Column is a wrapper to create a new Column statement.
@@ -33,12 +29,17 @@ func On(left string, right string) stmt.On {
 	return stmt.NewOn(stmt.NewColumn(left), stmt.NewColumn(right))
 }
 
-// Condition is a wrapper to create a new Condition statement.
-func Condition(column string, operator ...types.LogicalOperator) stmt.Condition {
-	op := types.And
-	if len(operator) > 0 {
-		op = operator[0]
-	}
+// Condition is a wrapper to create a new Identifier statement.
+func Condition(column string) stmt.Identifier {
+	return stmt.NewIdentifier(column)
+}
 
-	return stmt.NewCondition(stmt.NewColumn(column), op)
+// And is a wrapper to create a new InfixExpression statement.
+func And(left stmt.Expression, right stmt.Expression) stmt.InfixExpression {
+	return stmt.NewInfixExpression(left, stmt.NewLogicalOperator(types.And), right)
+}
+
+// Or is a wrapper to create a new InfixExpression statement.
+func Or(left stmt.Expression, right stmt.Expression) stmt.InfixExpression {
+	return stmt.NewInfixExpression(left, stmt.NewLogicalOperator(types.Or), right)
 }
