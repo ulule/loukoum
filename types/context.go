f23940fad2c940fd233c7418c37f6dbd05b0911a
Thomas LE ROUX <thomas@leroux.io>
2017-11-29 14:54:44 +0100

    docs: add comment

--- types/context.go
+++ types/context.go
@@ -5,15 +5,21 @@ import (
 	"fmt"
 )
 
+// A Context is passed to a root stmt.Statement to generate a query.
 type Context struct {
 	buffer bytes.Buffer
 	values map[string]interface{}
 }
 
+// Write appends given subquery in context's buffer.
 func (ctx *Context) Write(query string) {
-	ctx.buffer.WriteString(query)
+	_, err := ctx.buffer.WriteString(query)
+	if err != nil {
+		panic("loukoum: cannot write on buffer")
+	}
 }
 
+// Bind adds given value in context's values.
 func (ctx *Context) Bind(value interface{}) {
 	idx := len(ctx.values) + 1
 	key := fmt.Sprintf("arg_%d", idx)
@@ -22,14 +28,17 @@ func (ctx *Context) Bind(value interface{}) {
 	ctx.Write(key)
 }
 
+// Query returns the underlaying query.
 func (ctx *Context) Query() string {
 	return ctx.buffer.String()
 }
 
+// Values returns the underlaying values of the query.
 func (ctx *Context) Values() map[string]interface{} {
 	return ctx.values
 }
 
+// NewContext creates a new Context instance.
 func NewContext() *Context {
 	return &Context{
 		values: make(map[string]interface{}),
