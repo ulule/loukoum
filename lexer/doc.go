db2a0dd348e0e564b471a099b9c16dea2406301c
Thomas LE ROUX <thomas@leroux.io>
2018-07-24 15:18:07 +0200

    doc: Add mention of package instability

---
+++ lexer/doc.go
@@ -0,0 +1,5 @@
+// Package lexer provides a lexer/scanner for SQL queries.
+// It takes a io.Reader as source which can then be tokenized through repeated calls to the Next method.
+//
+// However, keep in mind that this package is highly unstable and should be used with extreme caution.
+package lexer
---
+++ parser/doc.go
@@ -0,0 +1,5 @@
+// Package parser generates an AST using "stmt" package from SQL queries.
+// It also provides an analyzer that exports various informations by scanning statements.
+//
+// However, keep in mind that this package is highly unstable and should be used with extreme caution.
+package parser
